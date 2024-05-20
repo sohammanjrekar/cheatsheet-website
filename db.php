2fd733d873bcbf9e579e9a3a163ecf0d86e16870
sohammanjrekar <mrsohammanjrekar@gmail.com>
2024-05-20 14:16:44 +0530

    upload

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = "localhost";
-$dbuser = "root";
-$dbpassword = "";
-$dbname = "cheatbook";
-$conn = mysqli_connect($hostname, $dbuser, $dbpassword, $dbname);
-if (!$conn) {
-    die("Could not connect to database");
-}
